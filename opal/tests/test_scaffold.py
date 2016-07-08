72a8fb18fe5b4e0dd94f25daa3994c4662d913ad
fredkingham <fredkingham@gmail.com>
2016-07-08 11:42:32 +0100

    lets just default null booleans to checkboxes

--- opal/tests/test_scaffold.py
+++ opal/tests/test_scaffold.py
@@ -79,6 +79,21 @@ class FormRenderTestCase(OpalTestCase):
             '{% load forms %}\n{% checkbox  field="Colour.name"  %}'
         )
 
+    @patch.object(Colour, "build_field_schema")
+    def test_null_boolean_render(self, build_field_schema, lshift):
+        build_field_schema.return_value = {
+            'lookup_list': None,
+            'model': 'Colour',
+            'name': 'name',
+            'title': 'Name',
+            'type': 'null_boolean'
+        },
+        scaffold_path = ffs.Path(settings.PROJECT_PATH)/'scaffolding'
+        create_form_template_for(Colour, scaffold_path)
+        lshift.assert_called_once_with(
+            '{% load forms %}\n{% checkbox  field="Colour.name"  %}'
+        )
+
 
 @patch("ffs.Path.__lshift__")
 class RecordRenderTestCase(OpalTestCase):
